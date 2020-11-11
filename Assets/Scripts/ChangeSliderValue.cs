a43577e93d55bb813c4afd32dbfc6d06c53d0350
l1l6dusa <63962252+l1l6dusa@users.noreply.github.com>
2020-11-11 10:29:41 +0100

    init

---
+++ Assets/Scripts/ChangeSliderValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSliderValue : MonoBehaviour
+{
+    private Slider _slider;
+    private int _currentSliderValue;
+
+
+    [SerializeField] private int _minValue, _maxValue;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        _currentSliderValue = _maxValue;
+        _slider.maxValue = _maxValue;
+        _slider.value = _currentSliderValue;
+    }
+
+    public void OnButtonClickSliderValueChange(int newValue)
+    {
+        _currentSliderValue += newValue;
+        _slider.value = Mathf.Clamp(_currentSliderValue, _minValue, _maxValue);
+    }
+}
---
+++ Assets/Scripts/SliderColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderColorChanger : MonoBehaviour
+{
+    private Image _image;
+    
+    [SerializeField] private Slider _slider;
+
+    [SerializeField] private Color _lowHealthColor;
+
+    [SerializeField] private Color _fullHealthColor;
+    
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void OnButtonChangeColor()
+    {
+        _image.color = Color.Lerp(_lowHealthColor, _fullHealthColor, _slider.value/_slider.maxValue);
+    }
+}
